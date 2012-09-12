346d25dbe7ff496380b963b9b319fdc975cb575d
John Tyree <johntyree@gmail.com>
2012-09-12 21:43:53 +0200

    Functions for problem 3. Lab2.

---
+++ john/3/form2bool.hs
@@ -0,0 +1,23 @@
+
+module Form2Bool
+
+where
+
+import Week2
+
+none = and . map not
+
+contradiction :: Form -> Bool
+contradiction form = none . map (flip eval form) . allVals $ form
+
+
+tautology :: Form -> Bool
+tautology form = and . map (flip eval form) . allVals $ form
+
+
+entails :: Form -> Form -> Bool
+entails f1 f2 = tautology (Impl f1 f2)
+
+
+equiv :: Form -> Form -> Bool
+equiv f1 f2 = tautology (Cnj [(Impl f1 f2), (Impl f2 f1)])
